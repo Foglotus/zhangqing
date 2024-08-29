218da5cdda574e475d1332477420ae683336b456
ikd_205c <ikd_elaiza@foglotus.com>
2024-08-29 19:39:37 +0800

    feat: 暂存

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="container">
+    
+  </div>
+</template>
+
+<style scoped>
+  .container {
+    height: 100%;
+    width: 100%;
+    background-image: url('./assets/bg.jpg');
+    background-size: cover;
+    background-repeat: no-repeat;
+
+  }
+</style>
